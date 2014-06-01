c0fc937da9d5663109a683ce4f8a508e2ea4738f
zakiay <zakiay.su@gmail.com>
2014-06-01 15:07:05 -0400

    changed filenames

---
+++ ex25.py
@@ -0,0 +1,35 @@
+def break_words(stuff):
+	"""This function will break up words"""
+	words = stuff.split(' ')
+	return words
+
+def sort_words(words):
+	"""Sorts the words"""
+	return sorted(words)
+
+def print_first_word(words):
+	"""Prints the first word after popping it off"""
+	word = words.pop(0)
+	print word
+
+def print_last_word(words):
+	"""Prints the last word after popping it off"""
+	work = words.pop(-1)
+	print word
+
+def sort_sentence(sentence):
+	"""Returns sorted sentence"""
+	words = break_words(sentence)
+	return sort_words(words)
+
+def print_first_and_last(sentence):
+	"""Prints first and last words"""
+	words = break_words(sentence)
+	print_first_word(words)
+	print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+	"""Sorts and prints first and last words"""
+	words = sort_sentence(sentence)
+	print_first_word(words)
+	print_last_word(words)
